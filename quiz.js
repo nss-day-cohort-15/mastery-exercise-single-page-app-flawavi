38b3246d093c72071cd50ff843e3211e64f3fb62
Fletcher Watson <fbwthe6@gmail.com>
2016-08-09 14:15:45 -0500

    fixed click problem, put cards in their own rows of three columns.

--- quiz.js
+++ quiz.js
@@ -1,4 +1,4 @@
-// var CarLot = (function (lot) {
+"use strict"// var CarLot = (function (lot) {
 
 //   lot.resetStyles = function (element) {
 
@@ -17,12 +17,16 @@ CarLot = (function (styles) {
   console.log(styles)
   var textInput = document.getElementById("text");
   var carCard = document.getElementsByClassName("carCard");
-
-  styles.populatePage = function () {
   var inventory = CarLot.getInventory();
+
+  styles.populatePage = function (inventory) {
+  var divControl = ""
+  var addToDom = document.getElementById("displayCars")
   for (var i = 0; i < inventory.length; i++) {
-    var addToDom = document.getElementById("displayCars")
-    addToDom.innerHTML += `
+    if (i % 3 === 0) {
+      divControl += `<div class="row">`
+    }
+    divControl += `
     <div id="${inventory[i].model}" class="col-sm-4 borderToggle carCard" style="border-color: ${inventory[i].color}">
       <h1 class="makeModel">${inventory[i].make} ${inventory[i].model}</h1>
       <p class= "list-unstyled year">${inventory[i].year}</p>
@@ -30,21 +34,25 @@ CarLot = (function (styles) {
       <p class= "list-unstyled color">${inventory[i].color}</p>
       <p class= "list-unstyled purchased">${inventory[i].purchased}</p>
       <p class= "list-unstyled description">${inventory[i].description}</p>
-    </div>
-    `;
+    </div>`
+    if ((i + 1) % 3 === 0) {
+      divControl += `</div>`
+    };
   }
+    addToDom.innerHTML = divControl
 
   CarLot.activateEvents();
 
   }
     styles.toggleBorderAndBackground = function () {
     //instert docEl and color parameteters
-      var carCard = event.target;
+      var carCard = event.currentTarget;
+      console.log(carCard)
       textInput.disabled = false;
       styles.resetCarCards();
       styles.clearAndFocus();
       CarLot.bindText();
-      carCard.parentElement.classList = "altBorder col-sm-4 carCard";
+      carCard.classList = "altBorder col-sm-4 carCard";
     },
 
     styles.resetCarCards = function () {
